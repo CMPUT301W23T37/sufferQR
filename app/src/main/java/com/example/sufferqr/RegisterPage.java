b3b8196d38abff7a4f1a968ca942689f0e21a041
Nofacefreak <titusli64@yahoo.com>
2023-02-17 16:55:59 -0700

    edit profile page ui design

---
+++ app/src/main/java/com/example/sufferqr/RegisterPage.java
@@ -0,0 +1,14 @@
+package com.example.sufferqr;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+    }
+}
