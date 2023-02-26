054b052a401694c9d1560f8710cba6a39d013b51
“Nuuradin <nuuradinsj@gmail.com>
2023-02-26 13:30:54 -0700

    added emoji drawer

---
+++ app/src/main/java/com/example/sufferqr/EmojiDraw.java
@@ -0,0 +1,19 @@
+package com.example.sufferqr;
+
+
+/**
+ * This class will take in a QR code as an  in the form of a dictionary and return a visual representation of the
+ * QR code.
+ * @version 1.0
+ */
+public class EmojiDraw {
+    /**
+     * We will first have a template of a cirle for the emoji
+     */
+    public static void drawCircleTemplate() {
+        System.out.println("");
+    }
+
+
+
+}
