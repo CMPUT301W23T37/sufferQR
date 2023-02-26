5378d96f4ca2ca4a21f1baf9baf169998c8ad25f
“Nuuradin <nuuradinsj@gmail.com>
2023-02-26 14:12:10 -0700

    version 1.0

--- app/src/main/java/com/example/sufferqr/EmojiDraw.java
+++ app/src/main/java/com/example/sufferqr/EmojiDraw.java
@@ -11,7 +11,26 @@ public class EmojiDraw {
      * We will first have a template of a cirle for the emoji
      */
     public static void drawCircleTemplate() {
-        System.out.println("");
+        int radius = 5; // Set the radius of the circle
+        int centerX = 5; // Set the X coordinate of the center of the circle
+        int centerY = 5; // Set the Y coordinate of the center of the circle
+
+        // Loop through each row and column to draw the circle
+        for (int y = 0; y <= 10; y++) {
+            for (int x = 0; x <= 10; x++) {
+                double distance = Math.sqrt((x - centerX) * (x - centerX) + (y - centerY) * (y - centerY));
+                if (Math.abs(distance - radius) < 0.5) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        drawCircleTemplate();
     }
 
 
