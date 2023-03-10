7fd82579f3f10627ee4b0615f1442d0ef4adf97a
Nofacefreak <titusli64@yahoo.com>
2023-03-09 19:03:18 -0700

    user's UI test (user profile and edit profile)

--- app/src/main/java/com/example/sufferqr/UserProfile.java
+++ app/src/main/java/com/example/sufferqr/UserProfile.java
@@ -1,44 +1,23 @@
 package com.example.sufferqr;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.sufferqr.databinding.ActivityUserProfileBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.color.utilities.MathUtils;
-import com.google.android.material.color.utilities.Score;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.util.Util;
 
-import org.w3c.dom.Text;
-
-import java.lang.reflect.Type;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -126,7 +105,6 @@ public class UserProfile extends DrawerBase {
             public void onClick(View view) {
                 Intent i = new Intent(UserProfile.this, EditProfile.class);
                 startActivity(i);
-                finish();
             }
         });
     }
