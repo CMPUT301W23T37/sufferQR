774ecba6f865e171a48343e9a0b714e8bb595b0d
“Nuuradin <nuuradinsj@gmail.com>
2023-03-10 16:21:52 -0700

    draw emoji broken

--- app/src/main/java/com/example/sufferqr/EmojiDraw.java
+++ app/src/main/java/com/example/sufferqr/EmojiDraw.java
@@ -1,120 +1,134 @@
 package com.example.sufferqr;
+
 import java.util.HashMap;
 
-/**
- * This is a class will take a qrcode as input in the form of a dictionary and create an emoji
- * composed of text with it
- */
 public class EmojiDraw {
-    private String qrcode;
-    private String[] bits;
-    private HashMap<String, String[]> dict;
+    private final String qrHash;
+    private final HashMap<String, String[]> featuresDict;
 
-    public EmojiDraw(String qrcode) {
-        this.qrcode = qrcode;
-        this.bits = qrcodeToBits(qrcode);
-        this.dict = createDict();
+    public EmojiDraw(String qrHash) {
+        this.qrHash = qrHash;
+        this.featuresDict = initFeaturesDict();
+    }
+
+    // Initialize the dictionary with all the possible facial features
+    private HashMap<String, String[]> initFeaturesDict() {
+        HashMap<String, String[]> dict = new HashMap<>();
+
+        // Shapes
+        dict.put("00", new String[]{"*******", "*     *", "*     *", "*     *", "*     *", "*     *", "*******"});
+        dict.put("01", new String[]{"  ***  ", " *   * ", "*     *", "*     *", "*     *", " *   * ", "  ***  "});
+
+        // Eyebrows
+        dict.put("00", new String[]{"     ", "=====", "     ", "     "});
+        dict.put("01", new String[]{"     ", "     ", "     ", "︵︵︵︵"});
+        dict.put("10", new String[]{"\\    ", "     ", "     ", "     "});
+        dict.put("11", new String[]{"//   ", "     ", "     ", "     "});
+
+        // Eyes
+        dict.put("00", new String[]{"  >  ", "     ", "     ", "     "});
+        dict.put("01", new String[]{"  <  ", "     ", "     ", "     "});
+        dict.put("10", new String[]{"  ^  ", "     ", "     ", "     "});
+        dict.put("11", new String[]{"  3  ", "     ", "     ", "     "});
+
+        // Nose
+        dict.put("00", new String[]{"     ", " / \\ ", "/   \\", "\\   /", " \\ / ", "     ", "     "});
+        dict.put("01", new String[]{"  O  ", "     ", "     ", "     "});
+        dict.put("10", new String[]{"  •  ", "     ", "     ", "     "});
+        dict.put("11", new String[]{"  [] ", "     ", "     ", "     "});
+
+        // Ears
+        dict.put("00", new String[]{" \\  ", "@   ", " \\  ", "     "});
+        dict.put("01", new String[]{" \\  ", "8   ", " /  ", "     "});
+        dict.put("10", new String[]{" \\  ", "%   ", "/   ", "     "});
+        dict.put("11", new String[]{" \\  ", "$   ", "\"   ", "     "});
+
+        // Mustache
+        dict.put("00", new String[]{"      VVVVVVVV      ", "                    "});
+        dict.put("01", new String[]{"      ########      ", "                    "});
+        dict.put("10", new String[]{"                      ", "                    "});
+        dict.put("11", new String[]{"      -----------       ", "                    "});
+
+        // Mouth
+        dict.put("00", new String[]{"              [=====]              ", "                    "});
+        dict.put("01", new String[]{"              (┬─┬)                   ", "                    "});
+        dict.put("10", new String[]{"              {┻━┻)                    ", "                    "});
+        dict.put("11", new String[]{"              |+++++|              ", "                   "});
+
+        // Moustache
+        dict.put("00", new String[]{" ", " VVVVV", " ", " "});
+        dict.put("01", new String[]{" ", "######", " ", " "});
+        dict.put("10", new String[]{" ", " ", " ", " "});
+        dict.put("11", new String[]{" ", "------", " ", " "});
+
+        // Mouth
+        dict.put("00", new String[]{"      ", "      ", "[=====]", "      "});
+        dict.put("01", new String[]{"      ", "(┬─┬)", "      ", "      "});
+        dict.put("10", new String[]{"      ", "{┻━┻)", "      ", "      "});
+        dict.put("11", new String[]{"      ", "|++++|", "      ", "      "});
+
+        // Beard
+        dict.put("0", new String[]{"      ", "      ", "      ", "      ", "      ", "      ", "VVVVV "});
+        dict.put("1", new String[]{"      ", "      ", "      ", "      ", "      ", "      ", "      "});
+
+        return dict;
+    }
+
+    private String getFeature(String bits, String[] options) {
+        int index = Integer.parseInt(bits, 2) % options.length;
+        return options[index];
     }
 
     public void draw() {
-        String head = dict.get("head")[Integer.parseInt(bits[0], 2)];
-        String eyebrows = dict.get("eyebrows")[Integer.parseInt(bits[1], 2)];
-        String eyes = dict.get("eyes")[Integer.parseInt(bits[2], 2)];
-        String nose = dict.get("nose")[Integer.parseInt(bits[3], 2)];
-        String facialHair = dict.get("facialHair")[Integer.parseInt(bits[4], 2)];
-        String mouth = dict.get("mouth")[Integer.parseInt(bits[5], 2)];
-        String ears = dict.get("ears")[Integer.parseInt(bits[6], 2)];
-        String[] headLines = head.split("\n");
-        int headWidth = headLines[0].length();
-        int headHeight = headLines.length;
-        int centerX = headWidth / 2;
-        int centerY = headHeight / 2;
-        int startX = centerX - (ears.length() > 6 ? 2 : 1);
-        int endX = centerX + (ears.length() > 6 ? 2 : 1);
-        for (int y = 0; y < headHeight; y++) {
-            for (int x = 0; x < headWidth; x++) {
-                if ((x >= startX && x <= endX) && (y == 0 || y == headHeight - 1)) {
-                    System.out.print(ears.charAt(x - startX));
-                } else if (x > 0 && x < headWidth - 1 && y > 0 && y < headHeight - 1) {
-                    if (y == 1 && x > 1 && x < headWidth - 2) {
-                        System.out.print(eyebrows.charAt(x - 2));
-                    } else if (y == centerY - 1 && x == centerX - 1) {
-                        System.out.print(eyes.charAt(0));
-                    } else if (y == centerY - 1 && x == centerX) {
-                        System.out.print(eyes.charAt(1));
-                    } else if (y == centerY - 1 && x == centerX + 1) {
-                        System.out.print(eyes.charAt(2));
-                    } else if (y == centerY && x == centerX - 1) {
-                        System.out.print(nose.charAt(0));
-                    } else if (y == centerY && x == centerX) {
-                        System.out.print(facialHair.charAt(0));
-                    } else if (y == centerY && x == centerX + 1) {
-                        System.out.print(nose.charAt(1));
-                    } else if (y == centerY + 1 && x == centerX) {
-                        System.out.print(mouth);
-                    } else {
-                        System.out.print(headLines[y].charAt(x));
-                    }
-                } else {
-                    System.out.print(headLines[y].charAt(x));
-                }
-            }
-            System.out.println();
+        String shapeBits = qrHash.substring(0, 2);
+        String shape = getFeature(shapeBits, featuresDict.get(shapeBits));
+
+        boolean isTriangle = shapeBits.charAt(0) == '0';
+        int noseIndex = Integer.parseInt(qrHash.substring(4, 6), 2) % 4;
+        int mouthIndex = Integer.parseInt(qrHash.substring(14, 15), 2);
+        String beard = getFeature(qrHash.substring(15, 16), featuresDict.get(Integer.toString(mouthIndex)));
+        String[] head = new String[7];
+        if (isTriangle) {
+            head[0] = "     *   ";
+            head[1] = "    * *  ";
+            head[2] = "   *   * ";
+            head[3] = "  *     *";
+            head[4] =     shape;
+            head[5] = "*        *";
+            head[6] = "*        *";
+        } else {
+            head[0] = "*******";
+            head[1] = "*     *";
+            head[2] = "*     *";
+            head[3] = "*     *";
+            head[4] =   shape;
+            head[5] = "*     *";
+            head[6] = "*******";
         }
+
+        String[] leftEar = featuresDict.get(qrHash.substring(6, 8));
+        String[] rightEar = featuresDict.get(qrHash.substring(8, 10));
+        String[] eyebrows = featuresDict.get(qrHash.substring(2, 4));
+        String[] eyes = featuresDict.get(qrHash.substring(10, 12));
+        String[] nose = featuresDict.get(Integer.toString(noseIndex));
+        String[] mustache = featuresDict.get(qrHash.substring(12, 14));
+        String[] mouth = featuresDict.get(Integer.toString(mouthIndex));
+
+        int noseY = isTriangle ? 4 : 3;
+
+        // Draw face
+        for (int i = 0; i < 7; i++) {
+            String leftEarLine = i == noseY ? leftEar[0] : " ";
+            String rightEarLine = i == noseY ? rightEar[0] : " ";
+            System.out.print(leftEarLine + " " + eyebrows[i] + " " + eyes[i] + " " + nose[i] + " " + eyes[i] + " " + eyebrows[i] + " " + rightEarLine + "\n");
+        }
+        System.out.print(" " + mustache[0] + beard + mustache[0] + "\n");
+        System.out.print(" " + mouth[0] + "\n");
     }
 
-    private String[] qrcodeToBits(String qrcode) {
-        // Split qrcode into 7-bit chunks
-        String[] chunks = qrcode.split("(?<=\\G.{7})");
-        // Convert each chunk to binary string
-        String[] bits = new String[chunks.length];
-        for   (int i = 0; i < chunks.length; i++) {
-            bits[i] = Integer.toBinaryString(Integer.parseInt(chunks[i], 16));
-            // Add leading zeros if necessary
-            bits[i] = "0".concat(bits[i]);
-            while (bits[i].length() < 8) {
-                bits[i] = "0".concat(bits[i]);
-            }
-        }
-        return bits;
-    }
 
-    private HashMap<String, String[]> createDict() {
-        HashMap<String, String[]> dict = new HashMap<>();
-        dict.put("head", new String[] {
-                "  /\\  \n" +
-                        " /  \\ \n" +
-                        "/____\\",
-                "  ___ \n" +
-                        " / _ \\\n" +
-                        "( (_) )\n" +
-                        " \\___/",
-                "   /\\  \n" +
-                        "  /  \\ \n" +
-                        " /____\\"
-        });
-        dict.put("eyebrows", new String[] {
-                "\\   /", "---   ---", "### ###"
-        });
-        dict.put("eyes", new String[] {
-                "X     X", "0     0", "3     3"
-        });
-        dict.put("nose", new String[] {
-                "W", "O", "\\/"
-        });
-        dict.put("facialHair", new String[] {
-                "", "=======", "VVVVVVV"
-        });
-        dict.put("mouth", new String[] {
-                "*", ":P"
-        });
-        dict.put("ears", new String[] {
-                "**  **", "()  ()", ""
-        });
-        return dict;
-    }
+
 }
-
 
 
 
