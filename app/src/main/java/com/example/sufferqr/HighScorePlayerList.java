cd146f883df87f8df38eb95f4252e061a9601072
meishahuide <932776527@qq.com>
2023-03-20 20:36:56 -0600

    high score player and high score qrcodes

--- app/src/main/java/com/example/sufferqr/HighScorePlayerList.java
+++ app/src/main/java/com/example/sufferqr/HighScorePlayerList.java
@@ -11,38 +11,40 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HighScorePlayerList extends ArrayAdapter<HighScorePlayer> {
+    public HighScorePlayerList(Context context, List<HighScorePlayer> highScorePlayerArrayList){
+        super(context,0,highScorePlayerArrayList);
 
-    private ArrayList<HighScorePlayer> highScorePlayers;
-    private Context context;
-
-    public HighScorePlayerList(Context context, ArrayList<HighScorePlayer> highScorePlayer){
-        super(context,0,highScorePlayer);
-        this.highScorePlayers = highScorePlayer;
-        this.context = context;
     }
-
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
-        View view = convertView;
-
-        if(view == null){
-            view = LayoutInflater.from(context).inflate(R.layout.high_score_player_content, parent,false);
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.rank_detail,
+                    parent, false);
+        } else {
+            view = convertView;
         }
 
-        HighScorePlayer highScorePlayer = highScorePlayers.get(position);
-        TextView rank = view.findViewById(R.id.higher_score_player_rank_text);
-        TextView userName = view.findViewById(R.id.higher_score_player_username_text);
-        TextView score = view.findViewById(R.id.higher_score_player_score_text);
+        HighScorePlayer highScorePlayer = getItem(position);
+        TextView rank = view.findViewById(R.id.rank);
+        TextView userName = view.findViewById(R.id.total_fourth_username);
+        TextView score = view.findViewById(R.id.total_fourth_score);
+
         int intRank = highScorePlayer.getRank();
         String strRank = Integer.toString(intRank);
         rank.setText(strRank);
-        userName.setText("username: " + highScorePlayer.getUsername());
-        score.setText("score: " + highScorePlayer.getScore());
+
+        userName.setText(highScorePlayer.getUsername());
+
+        int intScore = highScorePlayer.getScore();
+        String strScore = Integer.toString(intScore);
+        score.setText(strScore);
 
         return view;
 
--- app/src/main/java/com/example/sufferqr/Total.java
+++ app/src/main/java/com/example/sufferqr/Total.java
@@ -1,39 +1,86 @@
 package com.example.sufferqr;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Total extends Fragment {
-    private ListView ranks;
-
+    FirebaseFirestore db;
+    private ListView highScorePlayerArrayList;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_total, container, false);
 
+        db = FirebaseFirestore.getInstance();
 
-        // Create sample data
-        List<user_rank> exampleData = new ArrayList<>();
-        exampleData.add(new user_rank("4", "username4", "700"));
-        exampleData.add(new user_rank("5", "username5", "400"));
+        TextView FirstUsername = view.findViewById(R.id.total_first_name);
+        TextView FirstScore = view.findViewById(R.id.total_first_score);
+        TextView SecondUsername = view.findViewById(R.id.total_second_name);
+        TextView SecondScore = view.findViewById(R.id.total_second_score);
+        TextView ThirdUsername = view.findViewById(R.id.total_third_name);
+        TextView ThirdScore = view.findViewById(R.id.total_third_score);
+
+        ArrayList<HighScorePlayer> Data = new ArrayList<>();
 
 
-        // Set up the adapter
-        rank_adapter adapter = new rank_adapter(requireContext(), exampleData);
+        Query query = db.collection("Player").orderBy("sumScore", Query.Direction.DESCENDING).limit(5);
 
-        // Set the adapter for the ListView
-        ranks = view.findViewById(R.id.ranks_listview);
-        ranks.setAdapter(adapter);
+        query.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable
+            FirebaseFirestoreException error) {
+                //Data.clear();
+                int i = 0;
+                for(QueryDocumentSnapshot doc: value) {
+                    Log.d("Sample", String.valueOf(doc.getData().get("name")));
+                    Log.d("Sample", String.valueOf(doc.getData().get("sumScore")));
+                    String name = (String) doc.getData().get("name");
+                    String score = (String) doc.getData().get("sumScore").toString();
+                    int intScore = Integer.valueOf(score);
+                    i += 1;
+                    int rank = i;
+                    if (i == 1) {
+                        FirstUsername.setText(name);
+                        FirstScore.setText(score);
+                    }
+                    else if (i == 2) {
+                        SecondUsername.setText(name);
+                        SecondScore.setText(score);
+                    }
+                    else if (i == 3) {
+                        ThirdUsername.setText(name);
+                        ThirdScore.setText(score);
+                    }
+                    else if (i > 3) {
+                        Data.add(new HighScorePlayer(rank,name,intScore));
+                    }
+                }
+                HighScorePlayerList adapter = new HighScorePlayerList(requireContext(), Data);
+                highScorePlayerArrayList = view.findViewById(R.id.ranks_listview);
+                highScorePlayerArrayList.setAdapter(adapter);
+            }
+        });
 
         return view;
 
