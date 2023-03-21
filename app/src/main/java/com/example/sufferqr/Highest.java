f94e919814b3c61db3c5962e2d5e9acd945832c1
Gilbertcy <cyang13000@gmail.com>
2023-03-21 14:48:01 -0600

    1

--- app/src/main/java/com/example/sufferqr/Highest.java
+++ app/src/main/java/com/example/sufferqr/Highest.java
@@ -14,6 +14,6 @@ public class Highest extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_highest, container, false);
+        return inflater.inflate(R.layout.fragment_total, container, false);
     }
 }
