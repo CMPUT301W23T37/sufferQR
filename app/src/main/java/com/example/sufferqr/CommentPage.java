60f3f591400280d71dc20bde4c00e702f4b93243
Yoochu <rchu3@ualberta.ca>
2023-03-25 21:45:03 -0600

    comment

--- app/src/main/java/com/example/sufferqr/CommentPage.java
+++ app/src/main/java/com/example/sufferqr/CommentPage.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -19,10 +20,17 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
+import timber.log.Timber;
 
 public class CommentPage extends AppCompatActivity {
 //    private ArrayList<QRQuickViewComment> dataList;
@@ -32,10 +40,13 @@ public class CommentPage extends AppCompatActivity {
     String qrName;
     String uName;
     String comment;
+    String comToDel;
     Button saveComment;
     Button cancelComment;
     Button deleteComment;
     EditText commentContent;
+    TextView comTitle;
+    TextView showCom;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     DocumentReference ref;
@@ -49,7 +60,8 @@ public class CommentPage extends AppCompatActivity {
         cancelComment = findViewById(R.id.cancel_add_comment_button);
         commentContent = findViewById(R.id.comment_content);
         deleteComment = findViewById(R.id.delete_comment_button);
-        deleteComment.setVisibility(View.INVISIBLE);
+        comTitle = findViewById(R.id.comment_page_title);
+        showCom = findViewById(R.id.comment_to_be_delete);
 
         // retrieve the qr code name
         if (savedInstanceState == null) {
@@ -58,9 +70,22 @@ public class CommentPage extends AppCompatActivity {
                 qrName = null;
             } else {
                 qrName = extras.getString("QRName");
+                comTitle.setText(extras.getString("title"));
+                comToDel = extras.getString("commentContent");
             }
         } else {
             qrName = (String) savedInstanceState.getSerializable("QRName");
+            comTitle.setText((String) savedInstanceState.getSerializable("title"));
+            comToDel = (String) savedInstanceState.getSerializable("commentContent");
+        }
+
+        if ("Add Comment".equals(comTitle.getText().toString())) {
+            deleteComment.setVisibility(View.INVISIBLE);
+            showCom.setVisibility(View.INVISIBLE);
+        } else {
+            saveComment.setVisibility(View.INVISIBLE);
+            commentContent.setVisibility(View.INVISIBLE);
+            showCom.setText(comToDel);
         }
 
         ref = db.collection("GameQrCode").document(qrName);
@@ -109,6 +134,24 @@ public class CommentPage extends AppCompatActivity {
         deleteComment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ref
+                        .collection("Comment")
+                        .whereEqualTo("comment", comToDel)
+                        .whereEqualTo("userName", uName)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        ref.collection("Comment").document(document.getId()).delete();
+                                    }
+                                } else {
+                                    Timber.tag(TAG).d(task.getException(), "Error getting documents: ");
+                                }
+                            }
+                        });
+                Toast.makeText(CommentPage.this, "Comment Deleted", Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
