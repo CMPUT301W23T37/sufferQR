904df9706902ba8c0939df8133a73edd31efba81
Markmak233 <90290156+markmak233@users.noreply.github.com>
2023-04-02 23:22:00 -0600

    distance test

--- app/src/test/java/com/example/sufferqr/MapsUnitTest.java
+++ app/src/test/java/com/example/sufferqr/MapsUnitTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -14,30 +14,20 @@ import com.google.android.gms.maps.model.LatLng;
 @RunWith(MockitoJUnitRunner.class)
 public class MapsUnitTest {
 
-    private MapsActivity mapsActivity;
+    @Mock
+    private LatLng location1 ;
 
     @Mock
-    private LatLng location1;
-
-    @Mock
-    private LatLng location2;
-
-    @Before
-    public void setUp() {
-        mapsActivity = new MapsActivity();
-    }
+    private LatLng location2 ;
 
     @Test
     public void testIsWithinOneKilometer_sameLocation_returnsTrue() {
         // Given
-        when(location1.latitude).thenReturn(12.9715987);
-        when(location1.longitude).thenReturn(77.5945627);
-
-        when(location2.latitude).thenReturn(12.9715987);
-        when(location2.longitude).thenReturn(77.5945627);
+        location1 = new LatLng(12.9715987,77.5945627);
+        location2 = new LatLng(12.9715987,77.5945627);
 
         // When
-        boolean result = mapsActivity.isWithinOneKilometer(location1, location2);
+        boolean result = MapsActivity.isWithinOneKilometer(location1, location2);
 
         // Then
         assertTrue(result);
@@ -46,14 +36,11 @@ public class MapsUnitTest {
     @Test
     public void testIsWithinOneKilometer_withinOneKilometer_returnsTrue() {
         // Given
-        when(location1.latitude).thenReturn(12.9715987);
-        when(location1.longitude).thenReturn(77.5945627);
-
-        when(location2.latitude).thenReturn(12.977396);
-        when(location2.longitude).thenReturn(77.590063);
+        location1 = new LatLng(12.9715987,77.5945627);
+        location2 = new LatLng(12.977396,77.590063);
 
         // When
-        boolean result = mapsActivity.isWithinOneKilometer(location1, location2);
+        boolean result = MapsActivity.isWithinOneKilometer(location1, location2);
 
         // Then
         assertTrue(result);
@@ -62,14 +49,11 @@ public class MapsUnitTest {
     @Test
     public void testIsWithinOneKilometer_outsideOneKilometer_returnsFalse() {
         // Given
-        when(location1.latitude).thenReturn(12.9715987);
-        when(location1.longitude).thenReturn(77.5945627);
-
-        when(location2.latitude).thenReturn(12.989643);
-        when(location2.longitude).thenReturn(77.577956);
+        location1 = new LatLng(12.9715987,77.5945627);
+        location2 = new LatLng(12.989643,77.577956);
 
         // When
-        boolean result = mapsActivity.isWithinOneKilometer(location1, location2);
+        boolean result = MapsActivity.isWithinOneKilometer(location1, location2);
 
         // Then
         assertFalse(result);
